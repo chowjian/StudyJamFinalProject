4319d2b10fe62515bebfb0d3b04bace639d7f47b
Chow Jian <chow_jian@hotmail.com>
2016-04-19 20:53:14 +0800

    Initial upload

---
+++ app/src/main/java/jam/study/a5waystomakeeggs/FirstFragment.java
@@ -0,0 +1,66 @@
+package jam.study.a5waystomakeeggs;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by chong on 4/19/2016.
+ */
+public class FirstFragment extends Fragment {
+
+    private Button button;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.first_frag, container, false);
+
+        button = (Button) v.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                if (isNetworkAvailable()) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube://wdasrVE5NOc"));
+                    startActivity(intent);
+                }
+            }
+        });
+
+        return v;
+    }
+
+    /**
+     *  Creating an instance for class FirstFragment
+     * @return The instance of FirstFragment
+     */
+    public static FirstFragment newInstance() {
+
+        FirstFragment f = new FirstFragment();
+
+        return f;
+    }
+
+    /**
+     * Check if there is internet connection or not
+     * @return The result of the internet connectivity
+     */
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        if (activeNetworkInfo == null) {
+            Toast.makeText(getContext().getApplicationContext(), "You need a network connection.", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/jam/study/a5waystomakeeggs/MainActivity.java
@@ -0,0 +1,59 @@
+package jam.study.a5waystomakeeggs;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
+        pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+    }
+
+    private class MyPagerAdapter extends FragmentPagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int pos) {
+            switch(pos) {
+
+                case 0: return FirstFragment.newInstance();
+                case 1: return SecondFragment.newInstance();
+                case 2: return ThirdFragment.newInstance();
+                case 3: return FourthFragment.newInstance();
+                case 4: return FifthFragment.newInstance();
+                default: return FirstFragment.newInstance();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+    }
+}
